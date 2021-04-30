7596243be695a7ffe861c440a0a6dd20e56b41af
Flynn Lambrechts <flynnlambrechts@Flynns-MacBook-Pro.local>
2021-04-30 20:04:29 +1000

    made entity and value global variables

--- utils.py
+++ utils.py
@@ -12,18 +12,21 @@ client = Wit(access_token = wit_access_token)
 #collects what the entity is e.g. mealtype
 # and its value e.g. breakfast, lunch or dinner
 def wit_response(message): #prev message_text
-	resp = client.message(message) #prev message_text
-	
-	entity = None
-	value = None
+        resp = client.message(message) #prev message_text
+        global entity, value
+        entity = None
+        value = None
 
-	try:
-		entity = list(resp['entities'])[0]
-		value = resp['entities'][entity][0]['value']
-	except:
-		pass
+        try:
+                entity = list(resp['entities'])[0]
+                value = resp['entities'][entity][0]['value']
+        except:
+                pass
 
-	return (entity, value)
+        return (entity, value)
 
 #test for random question:
-#print(wit_response("what is breakfast"))
+
+print(wit_response("what is breakfast"))
+print(entity)
+print(value)
