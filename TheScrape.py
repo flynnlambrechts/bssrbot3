5b68dd67b2afaf3d66a70d0e91ce14f68dd62873
Flynn Lambrechts <flynnlambrechts@Flynns-MBP.modem>
2021-05-02 20:59:20 +1000

    put the html opening inside info func

--- TheScrape.py
+++ TheScrape.py
@@ -105,28 +105,30 @@ def checkForDino(message):
     return response
 
 
-# Opening the html file
-HTMLFile = open(str(page + ".html"), "r") #try putting in func.
-
-  
-# Reading the file
-index = HTMLFile.read()
-  
-# Creating a BeautifulSoup object and specifying the parser
-soup = BeautifulSoup(index, 'lxml')
-
-# Using the prettify method to modify the code
-#print(soup.body.prettify())
-
-#print(soup.title) #prints the table title if it has one
-
-menu_table = soup.find("table", attrs={"class": "dataframe"})
-menu_table_data = menu_table.tbody.find_all("tr")  # contains 2 rows
-
-# Get all the headings of Lists
 
 def getinfo():
     global column
+    global page
+    
+    ################## # Opening the html file
+    HTMLFile = open(str(page + ".html"), "r") #try putting in func.
+
+    # Reading the file
+    index = HTMLFile.read()
+      
+    # Creating a BeautifulSoup object and specifying the parser
+    soup = BeautifulSoup(index, 'lxml')
+
+    # Using the prettify method to modify the code
+    #print(soup.body.prettify())
+
+    #print(soup.title) #prints the table title if it has one
+
+    menu_table = soup.find("table", attrs={"class": "dataframe"})
+    menu_table_data = menu_table.tbody.find_all("tr")  # contains 2 rows
+    ##################
+
+    
     info = []
     for td in menu_table_data[row].find_all("td"):
         if td is not None:
