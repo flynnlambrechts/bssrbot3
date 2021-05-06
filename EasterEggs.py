04a786858e8752ba8a9129b8a2f6b7cf6426b769
Flynn Lambrechts <flynnlambrechts@Flynns-MacBook-Pro.local>
2021-05-06 19:28:58 +1000

    imported random

--- EasterEggs.py
+++ EasterEggs.py
@@ -1,6 +1,8 @@
+import random
+
 def checkForEasterEggs(message):
     response = ""
-    if "updog1" in message:
+    if "updog" in message:
         response = response + "What is updog?"
     elif message == "random":
         sample_responses = ["ben is a cockboy","molly farted","crispy is a simp","mitchy is thick","hugo is sick"]
