ef867dd01abb698aed9f219e38788f54d6c95156
Flynn Lambrechts <flynnlambrechts@Flynns-MacBook-Pro.local>
2021-05-06 19:25:48 +1000

    slight fix

--- EasterEggs.py
+++ EasterEggs.py
@@ -1,7 +1,7 @@
 def checkForEasterEggs(message):
     response = ""
     if "updog1" in message:
-        response = response + "Room points are a lie!")
+        response = response + "What is updog?"
     elif message == "random":
         sample_responses = ["ben is a cockboy","molly farted","crispy is a simp","mitchy is thick","hugo is sick"]
         response = response + random.choice(sample_responses)
