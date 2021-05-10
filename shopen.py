ade9765df69e1335f0843fc8982fc7c2f0bcc0ee
Flynn Lambrechts <flynnlambrechts@Flynns-MacBook-Pro.local>
2021-05-10 19:14:09 +1000

    shopen stopped wokring

--- shopen.py
+++ shopen.py
@@ -111,6 +111,7 @@ def timeTillClose(end_time):
     current_time = datetime.datetime.now(TIMEZONE).strftime('%Y-%m-%d %H:%M:%S')
     close_time = datetime.datetime.strptime(str(end_time),'%Y-%m-%d %H:%M:%S')
     remaining_time = close_time - datetime.datetime.strptime(str(current_time),'%Y-%m-%d %H:%M:%S')
+    print(remaining_time)
     return remaining_time
 
 def get_shopen():
