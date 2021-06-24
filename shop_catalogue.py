29d63163677bb3cef444c726112d530d4749a071
Flynn Lambrechts <flynnlambrechts@Flynns-MacBook-Pro.local>
2021-06-24 14:55:04 +1000

    new stuff

--- shop_catalogue.py
+++ shop_catalogue.py
@@ -7,5 +7,17 @@ Tee Vee Snack $3\n\
 Tiny Teddies $5 \U0001f43b\n\
 Coke $1 \U0001f964\n\
 Up n Go $ 1\n\
-Drumstick $ 1\U0001f366"
+Drumstick $ 1\U0001f366\n\
+Maggi Noodles $1\n\
+Tonic Water $1\n\
+10 Pack Twinings Tea $1\n\
+Oreo $1.5\n\
+Tim Tams $2\n\
+Maxi Bon $2\n\
+Red Bull $2\n\
+Pads $2\n\
+Meegs $2.5\n\
+Shapes $2.5\n\
+Cadbury Blocks $2.5\n\
+Red Rock Deli Chips $3"
 
