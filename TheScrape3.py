c757d76382a8da75fc8fb356a9308f11d088e745
flynnlambrechts <flynn.lambrechts@gmail.com>
2021-09-14 11:30:20 +1000

    cant spell

--- TheScrape3.py
+++ TheScrape3.py
@@ -119,7 +119,7 @@ def getDino(message, value, recipient_id, con=None):
 				if recipient_id in Staff_ID:
 					response = " ".join([response, str(daysuntil(future)), "Days until freedom..."])
 				else:
-					response = " ".join([response, str(daysuntil(future)), "Days until a Regeant Rumsie 🍺🍺"])
+					response = " ".join([response, str(daysuntil(future)), "Days until a Regent Rumsie 🍺🍺"])
 			else:
 				print(False)
 		return response
