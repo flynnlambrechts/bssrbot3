9505231517a0f62f5a4fef2726be7e3a08efcf9e
Flynn Lambrechts <flynn.lambrechts@student.unsw.edu.au>
2022-04-08 22:45:01 +1000

    Starting out

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-  
-#Python libraries that we need to import for our bot
-import os, sys                          #for heroku env
-import psycopg2                         #database stuff
+
+# Python libraries that we need to import for our bot
+import os
+import sys  # for heroku env
+import psycopg2  # database stuff
 import json
 
-from flask import Flask, request        #flask
+from flask import Flask, request  # flask
 
 from response import Response
 from get_bot_response import get_bot_response
@@ -15,23 +16,25 @@ from bot_functions import (PrintException, getCon)
 app = Flask(__name__)
 
 #Developer: Flynn
-#Contributors: Ethan, Jas, Zoe
+# Contributors: Ethan, Jas, Zoe
 
-#We will receive messages that Facebook sends our bot at this endpoint 
+# We will receive messages that Facebook sends our bot at this endpoint
+
+
 @app.route("/", methods=['GET', 'POST'])
 def receive_message():
     if request.method == 'GET':
         """Before allowing people to message your bot, Facebook has implemented a verify token
-        that confirms all requests that your bot receives came from Facebook.""" 
+        that confirms all requests that your bot receives came from Facebook."""
         token_sent = request.args.get("hub.verify_token")
         return verify_fb_token(token_sent)
-    #if the request was not get, it must be POST and we can just proceed with sending a message back to user
+    # if the request was not get, it must be POST and we can just proceed with sending a message back to user
     else:
         # get whatever message a user sent the bot
         output = request.get_json()
     try:
         if output:
-            #log(output) #entire output good for finding sender ids what message contains etc
+            # log(output) #entire output good for finding sender ids what message contains etc
             for event in output['entry']:
                 messaging = event['messaging']
                 for message in messaging:
@@ -63,20 +66,25 @@ def receive_message():
             response = Response(ID)
             response.text = "Oh no something went wrong :("
             response.send()
-        #maybe add func to text admin when error occurs
+        # maybe add func to text admin when error occurs
     return "Message Processed"
 
+@app.route("/upload_dino_image", methods=['POST'])
+def upload_image():
+    return
 
 def log(message):
     print(message)
     sys.stdout.flush()
-    
+
+
 def verify_fb_token(token_sent):
-    #take token sent by facebook and verify it matches the verify token you sent
-    #if they match, allow the request, else return an error 
+    # take token sent by facebook and verify it matches the verify token you sent
+    # if they match, allow the request, else return an error
     if token_sent == VERIFY_TOKEN:
         return request.args.get("hub.challenge")
     return 'Invalid verification token'
+
 
 if __name__ == "__main__":
     app.run()
--- bot_constants.py
+++ bot_constants.py
@@ -1,31 +1,31 @@
-#Constants
+# Constants
 import os
 from pytz import timezone
 
 TIMEZONE = timezone('Australia/Sydney')
 
-ACCESS_TOKEN = os.environ['ACCESS_TOKEN'] #used for fb connection
+ACCESS_TOKEN = os.environ['ACCESS_TOKEN']  # used for fb connection
 ACCESS_TOKEN_BACKUP = os.environ['ACCESS_TOKEN_BACKUP']
-VERIFY_TOKEN = os.environ['VERIFY_TOKEN'] #used to verify fb
+VERIFY_TOKEN = os.environ['VERIFY_TOKEN']  # used to verify fb
 DATABASE_URL = os.environ['DATABASE_URL']
 HOLIDAY_MODE = False
 
-Admin_ID = ["4409117335852974", #Flynn-DEV
-            "3760608700732342" #Flynn-REAL
-] #id of users with powerful permission
+Admin_ID = ["4409117335852974",  # Flynn-DEV
+            "3760608700732342"  # Flynn-REAL
+            ]  # id of users with powerful permission
 
-Staff_ID = ["3963292617085764", #Kendy
-"4294576107270093", #Sam
-"4610989705596963", #Kyra
-"3890926854296476", #James
-"4522684851127792" #mum
-]
+Staff_ID = ["3963292617085764",  # Kendy
+            "4294576107270093",  # Sam
+            "4610989705596963",  # Kyra
+            "3890926854296476",  # James
+            "4522684851127792"  # mum
+            ]
 
-dino_quickreplies = [{'content_type': 'text', 'title': 'Breakfast', 'payload': 'Breakfast'},\
- {'content_type': 'text', 'title': 'Lunch', 'payload': 'Lunch'},\
- {'content_type': 'text', 'title': 'Dinner', 'payload': 'Dinner'},\
- {'content_type': 'text', 'title': 'Dino', 'payload': 'Dino'}]
- #{'content_type': 'text', 'title': 'All', 'payload': 'All'}]
+dino_quickreplies = [{'content_type': 'text', 'title': 'Breakfast', 'payload': 'Breakfast'},
+                     {'content_type': 'text', 'title': 'Lunch', 'payload': 'Lunch'},
+                     {'content_type': 'text', 'title': 'Dinner', 'payload': 'Dinner'},
+                     {'content_type': 'text', 'title': 'Dino', 'payload': 'Dino'}]
+# {'content_type': 'text', 'title': 'All', 'payload': 'All'}]
 
 greeting_message = f"Hello! Welcome to BssrBot! I'm here to help you with all your dino and calendar needs.\
 Here are some example questions:\
@@ -58,29 +58,27 @@ Cadbury Blocks $2.5\n\
 Red Rock Deli Chips $3"
 
 # #Basser used in get_bot_response
-bassertimes = {"breakfast": "7:30-10:00am",\
-"lunch" : "12:00-1:00pm",\
-"dinner" : "5:00-6:15pm",\
-"seconds" : "6:30-6:45pm, 7:45-8:00pm"}
-dinotimes = "".join(["Dino Times: \nBreakfast: ", bassertimes["breakfast"], \
-    "\nLunch: ", bassertimes["lunch"], \
-    "\nDinner: ", bassertimes["dinner"],\
-    "\n\nSeconds: ", bassertimes["seconds"]])
+bassertimes = {"breakfast": "7:30-10:00am",
+               "lunch": "12:00-1:00pm",
+               "dinner": "5:00-6:15pm",
+               "seconds": "6:30-6:45pm, 7:45-8:00pm"}
+dinotimes = "".join(["Dino Times: \nBreakfast: ", bassertimes["breakfast"],
+                     "\nLunch: ", bassertimes["lunch"],
+                     "\nDinner: ", bassertimes["dinner"],
+                     "\n\nSeconds: ", bassertimes["seconds"]])
 
-#no longer used this was a COVID thing
-notbassertimes = {"Baxter" : "Baxter Dino Times:\nBreakfast: 8:45-10:00am\nLunch: 1:15-2:15pm\nDinner: 6:15-7:15pm",\
-"Goldstein" : "Goldstein Dino Times:\nBreakfast: 7:30-8:30am\nLunch: 12:00-1:00pm\nDinner: 5:00-6:00pm",\
-"Fig" : "Fig Tree Dino Times:\nBreakfast: 7:30-8:30am\nLunch: 12:00-1:00pm\nDinner: 5:00-6:00pm",\
-"Hall" : "Hall Dino Times:\nBreakfast: As Normal \nDinner: 7:30-8:15pm"}
+# no longer used this was a COVID thing
+notbassertimes = {"Baxter": "Baxter Dino Times:\nBreakfast: 8:45-10:00am\nLunch: 1:15-2:15pm\nDinner: 6:15-7:15pm",
+                  "Goldstein": "Goldstein Dino Times:\nBreakfast: 7:30-8:30am\nLunch: 12:00-1:00pm\nDinner: 5:00-6:00pm",
+                  "Fig": "Fig Tree Dino Times:\nBreakfast: 7:30-8:30am\nLunch: 12:00-1:00pm\nDinner: 5:00-6:00pm",
+                  "Hall": "Hall Dino Times:\nBreakfast: As Normal \nDinner: 7:30-8:15pm"}
 
 
 #used in TheScrape3
-week_days = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
+week_days = ["Monday", "Tuesday", "Wednesday",
+             "Thursday", "Friday", "Saturday", "Sunday"]
 
-#DEFINED IN THESCRAPE3 IN CLASS UPDATE THERE
+# DEFINED IN THESCRAPE3 IN CLASS UPDATE THERE
 # breakfastheaders = [u"Residential Breakfast \U0001f95e", "Special"]
 # lunchheaders = [ u"Hot Option \U0001F37D", u"Vegetarian Option \U0001F331", u"Soup \U0001f372"]
 # dinnerheaders = [u"Main Course \U0001F37D", u"Vegetarian \U0001F331", u"Salad \U0001F957", "Vegetables", u"Additional Vegetables \U0001F966", u"The Dessert Station \U0001f370"]
-
-
-
