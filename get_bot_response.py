2e96767081bcaa7ce03cf3973c05ccef40cdd8a2
Flynn Lambrechts <flynn.lambrechts@student.unsw.edu.au>
2022-07-16 11:06:59 +1000

    Tabs and spaces

--- get_bot_response.py
+++ get_bot_response.py
@@ -30,25 +30,25 @@ bot.load_directory("./brain")
 bot.sort_replies()
 
 def set_vacuum(rs, location):
-    try:
-        psid = bot.current_user()
-        location = " ".join(location)
-        person = Sender(psid).get_fullname()
-        time_now = datetime.datetime.now(TIMEZONE)
-        print(time_now)
-        GlobalVar('vacuum').update({'index':1,'location':location,'person':person,'time':time_now})
-        return "Hope you had a good 'cuum. The location has been updated"
-    except:
-        PrintException()
-    
+	try:
+		psid = bot.current_user()
+		location = " ".join(location)
+		person = Sender(psid).get_fullname()
+		time_now = datetime.datetime.now(TIMEZONE)
+		print(time_now)
+		GlobalVar('vacuum').update({'index':1,'location':location,'person':person,'time':time_now})
+		return "Hope you had a good 'cuum. The location has been updated"
+	except:
+		PrintException()
+	
 
 def get_vacuum(rs, args):
-    row = GlobalVar('vacuum').get()
-    location = row[1]
-    person = row[2]
-    time = datetime.datetime.strptime(row[3], '%Y-%m-%d %H:%M:%S.%f%z')
-    time = time.strftime('%I:%M%p, %d %b')
-    return f"Vacuum Logs: \nLast Used by: {person} \nTime: {time} \nLocation left: {location}"
+	row = GlobalVar('vacuum').get()
+	location = row[1]
+	person = row[2]
+	time = datetime.datetime.strptime(row[3], '%Y-%m-%d %H:%M:%S.%f%z')
+	time = time.strftime('%I:%M%p, %d %b')
+	return f"Vacuum Logs: \nLast Used by: {person} \nTime: {time} \nLocation left: {location}"
 
 bot.set_subroutine("set_vacuum", set_vacuum)
 bot.set_subroutine("get_vacuum", get_vacuum)
@@ -111,16 +111,16 @@ def get_bot_response(recipient_id, message_text="", attachment = None):
 		response.text = getTime(message)
 
 	elif checkForDino(message):
-        try:
+		try:
 		# response.text = "Sorry y'all Basser Bot doesnt have the menu atm."
-            value = checkForDino(message)
-            con = getCon()
-            dino = getDino(message, value, recipient_id, con)
-            response.text = dino['text']
-            con.close()
-        except:
-            PrintException()
-            response.text = 'Something went wrong with the menu...'
+			value = checkForDino(message)
+			con = getCon()
+			dino = getDino(message, value, recipient_id, con)
+			response.text = dino['text']
+			con.close()
+		except:
+			PrintException()
+			response.text = 'Something went wrong with the menu...'
 
 		button = UrlButton("Latemeal","https://user.resi.inloop.com.au/home").get_button()
 		response.addbutton(button)
